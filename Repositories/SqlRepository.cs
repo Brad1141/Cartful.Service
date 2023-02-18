1a39b02e86fb1aa375fc65cd01b3cb9fdafebd4a
brad1141 <bradlamontagne1141@gmail.com>
2023-02-18 12:29:26 -0500

    added "get by id" & "add" to account repo

--- Repositories/SqlRepository.cs
+++ Repositories/SqlRepository.cs
@@ -1,20 +1,19 @@
 using System.Linq.Expressions;
+using System.Data.SqlClient;
+using Cartful.Service.Entities;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cartful.Service.Repositories
 {
-    public class SqlRepository<T> : IRepository<T>
+    public class accountRepository
     {
 
-        // variable which holds the instance of the collection
-        // readonly: the value of the field cannot be changed after it is initialized 
-        private readonly IMongoCollection<T> dbCollection;
+        SqlConnection _connection;
 
-        // filter builder which queries for items in mongoDB
-        private readonly FilterDefinitionBuilder<T> filterBuilder = Builders<T>.Filter;
-
-        public SqlRepository(IMongoDatabase database, string collectionName)
+        public accountRepository(SqlConnection connection)
         {
-            dbCollection = database.GetCollection<T>(collectionName);
+            this._connection = connection;
         }
 
         // calls to databases are often async because they take a long time to complete
@@ -31,28 +30,69 @@ namespace Cartful.Service.Repositories
         }
 
         // get entry by id
-        public async Task<T> GetAsync(Guid id)
+        public async Task<ActionResult<Account>> GetAsync(Guid id)
         {
-            // create an equality filter
-            FilterDefinition<T> filter = filterBuilder.Eq(entity => entity.Id, id);
-            // call the filter and return the first hit
-            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+            _connection.Open();
+
+            // create a SQL command to select data from a table based on its ID
+            string sql = "SELECT * FROM MyTable WHERE Id = @Id";
+            SqlCommand command = new SqlCommand(sql, _connection);
+            command.Parameters.AddWithValue("@Id", id);
+
+            // execute the command and retrieve the results
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                Account userAccount = new Account();
+                userAccount.firstName = reader.GetString(1);
+                userAccount.lastName = reader.GetString(2);
+                userAccount.userName = reader.GetString(3);
+                userAccount.password = reader.GetString(4);
+                userAccount.phoneNumber = reader.GetString(5);
+
+                reader.Close();
+                _connection.Close();
+
+                return userAccount;
+            }
+            else
+            {
+                return new NotFoundResult();
+            }
+
         }
-        
+
         public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
         {
             return await dbCollection.Find(filter).FirstOrDefaultAsync();
         }
 
         // add item to db
-        public async Task CreateAsync(T entity)
+        public async Task<IActionResult> CreateAsync(Account account)
         {
-            if (entity == null)
-            {
-                throw new ArgumentException(nameof(entity));
-            }
+            string sql = "INSERT INTO user (Column1, Column2, Column3, Column4, Column5, Column6) VALUES (@Value1, @Value2, @Value3, @Value4, @Value5, @Value6)";
+            SqlCommand command = new SqlCommand(sql, _connection);
 
-            await dbCollection.InsertOneAsync(entity);
+            // Set the values of the parameters
+            command.Parameters.AddWithValue("@Value1", account.userId);
+            command.Parameters.AddWithValue("@Value2", account.firstName);
+            command.Parameters.AddWithValue("@Value3", account.lastName);
+            command.Parameters.AddWithValue("@Value4", account.userName);
+            command.Parameters.AddWithValue("@Value5", account.password);
+            command.Parameters.AddWithValue("@Value6", account.phoneNumber);
+
+            // Execute the command
+            int rowsAffected = await command.ExecuteNonQueryAsync();
+
+            if (rowsAffected <= 0)
+            {
+                return new BadRequestResult();
+            }
+            else
+            {
+                return new OkResult();
+            }
         }
 
         public async Task UpdateAsync(T entity)
