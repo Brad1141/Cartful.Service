6a8d323e63caa939b13a3e44dae312f58fb03b73
brad1141 <bradlamontagne1141@gmail.com>
2023-02-18 10:27:40 -0500

    added create account feature. repos need work

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Cartful.Service.Dtos;
+using Cartful.Service.Entities;
+using Cartful.Service.Repositories;
+
+namespace Cartful.Service.Controllers;
+
+[ApiController]
+[Route("account")]
+public class AccountController : ControllerBase
+{
+
+    private readonly IRepository<Account> accountRepository;
+    public AccountController(IRepository<Account> accountRepository)
+    {
+        this.accountRepository = accountRepository;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AccountDto>> Post(AccountDto accountDto)
+    {
+        // generate guid
+        Guid newGuid = new Guid();
+
+        var newAccount = new Account
+        {
+            userId = newGuid,
+            firstName = accountDto.firstName,
+            lastName = accountDto.lastName,
+            password = accountDto.password,
+            phoneNumber = accountDto.phoneNumber
+            
+        };
+
+        await accountRepository.CreateAsync(newAccount);
+        return CreatedAtAction("New Account", new { userId = newAccount.userId }, newAccount);
+    }
+}
