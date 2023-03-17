7c9ea4b40d78c47b54d3e4b9d0d72872a7ccc759
brad1141 <bradlamontagne1141@gmail.com>
2023-03-16 23:09:45 -0400

    fixed CreateList function

--- Dtos.cs
+++ Dtos.cs
@@ -2,8 +2,8 @@ using Cartful.Service.Entities;
 
 namespace Cartful.Service.Dtos
 {
-    public record AccountDto(string firstName, string lastName, string userName, string password, string? phoneNumber);
+    public record AccountDto(string firstName, string lastName, string userName, string password, string phoneNumber);
 
-    public record ListDto(string title, string userID, List<Item> items);
+    public record ListDto(string title, Guid userID, List<Item> items);
 
 }
